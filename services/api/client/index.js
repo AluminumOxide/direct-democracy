f08e40bca8bd0901a946ddadd2d3569fb111ad9c
AluminumOxide <sapphyre.gervais+git@gmail.com>
2025-06-29 23:33:40 -0400

    issue 43: add auth to external api

--- services/api/client/index.js
+++ services/api/client/index.js
@@ -1,2 +1,74 @@
 const get_client = require('@aluminumoxide/direct-democracy-lib-client')
-module.exports = get_client(require('./spec.json'), require('./errors.json'))
+const server = get_client(require('./spec.json'), require('./errors.json'))
+const profile_client = require('@aluminumoxide/direct-democracy-profile-api-client')
+const account_client = require('@aluminumoxide/direct-democracy-account-api-client')
+const token_client = require('@aluminumoxide/direct-democracy-token-api-client')
+
+/* Sign Up */
+
+// returns: { salt }
+const sign_up_one = async function({ email, password, question }) {
+	return await account_client.client_sign_up_one({ email, password, question })
+}
+
+// returns: profile_token
+const sign_up_two = async function({ email_token }) {
+	return await token_client.clean_token(email_token)
+}
+
+// returns: { profile_id, signup_token }
+const sign_up_three = async function({ answer, profile_token }) {
+	const { profile_id, token: signup_token } =  await profile_client.client_sign_up({ answer, profile_token })
+	return { profile_id, signup_token }
+}
+
+// returns: account_token
+const sign_up_four = async function({ signup_token }) {
+	return await token_client.clean_token(signup_token)
+}
+
+// returns: nothing on success
+const sign_up_five = async function({ answer, salt, profile_id, email_token, account_token }) {
+	return await account_client.client_sign_up_two({ answer, salt, profile_id, email_token, account_token })
+}
+
+/* Sign In */
+
+// returns: { question, salt, encrypted_profile }
+const sign_in_one = async function({ email, password }) {
+	return await account_client.client_sign_in_one({ email, password })
+}
+
+// returns: profile_id
+const sign_in_two = async function({ answer, salt, encrypted_profile }) {
+	return await account_client.client_sign_in_two({ answer, salt, encrypted_profile })
+}
+
+// returns: { jwt }
+const sign_in_three = async function({ profile_id, answer }) {
+	return await profile_client.client_sign_in({ profile_id, answer })
+}
+
+/* Sign Out */
+const sign_out = async function({ jwt }) {
+	return await profile_client.sign_out({ jwt })
+}
+
+/* Verify */
+const verify_jwt = async function({ jwt }) {
+	return await profile_client.verify({ jwt })
+}
+
+module.exports = {
+	...server,
+	sign_up_one,
+	sign_up_two,
+	sign_up_three,
+	sign_up_four,
+	sign_up_five,
+	sign_in_one,
+	sign_in_two,
+	sign_in_three,
+	sign_out,
+	verify_jwt
+}
--- services/api/source/test/end-to-end.test.js
+++ services/api/source/test/end-to-end.test.js
@@ -1,39 +1,11 @@
 const {
 	sinon,
 	errors,
-	get_uuid,
-	get_first_uuid,
-	get_timestamp,
-	get_first_timestamp,
-	get_dummy_log,
-	get_dummy_reply,
-	get_dummy_db,
-	get_dummy_api,
 	integration_test_setup,
 	integration_test_query,
-	democracy_list_integration,
-	democracy_read_integration,
-	membership_list_integration,
-	membership_create_integration,
-	membership_read_integration,
-	membership_delete_integration,
-	proposal_list_integration,
-	proposal_list_public_integration,
-	proposal_create_integration,
-	proposal_read_integration,
-	proposal_read_public_integration,
-	proposal_delete_integration,
-	ballot_list_integration,
-	ballot_list_public_integration,
-	ballot_create_integration,
-	ballot_read_integration,
-	ballot_update_integration,
-	ballot_delete_integration
 } = require('./helper')
-
-const profile_client = require('@aluminumoxide/direct-democracy-profile-api-client')
-const account_client = require('@aluminumoxide/direct-democracy-account-api-client')
 const token_client = require('@aluminumoxide/direct-democracy-token-api-client')
+const api_client = require('@aluminumoxide/direct-democracy-external-api-client')
 
 describe('End-to-End Tests', () => {
 
@@ -53,66 +25,32 @@ describe('End-to-End Tests', () => {
 		const answer = 'test test test test.'
 
 		/* sign up */
-		const sign_up_one = async function({ email, password, question }) {
 
-			// account sign up
-			return await account_client.client_sign_up_one({ email, password, question })
-		}
-
-		const sign_up_two = async function({ email_token, answer, salt }) {
-
-			// clean email token
-			const profile_token = await token_client.clean_token(email_token)
-
-			// profile sign up
-			const { profile_id, token: signup_token } = await profile_client.client_sign_up({ answer, profile_token })
-
-			// clean signup token
-			const account_token = await token_client.clean_token(signup_token)
-
-			// complete account sign up
-			return await account_client.client_sign_up_two({ answer, salt, profile_id, email_token, account_token })
-		}
-
-		let su = await sign_up_one({ email, password, question })
-		su.email_token = email_token
-		su.answer = answer
-		await sign_up_two(su)
+		const su1 = await api_client.sign_up_one({ email, password, question })
+		const su2 = await api_client.sign_up_two({ email_token })
+		const su3 = await api_client.sign_up_three({ answer, profile_token: su2 })
+		const su4 = await api_client.sign_up_four({ signup_token: su3.signup_token })
+		await api_client.sign_up_five({ answer, salt: su1.salt, profile_id: su3.profile_id, email_token, account_token: su4 })
+		
 
 		/* sign in */
 
-		const sign_in_one = async function({ email, password }) {
-
-			// sign in account
-			return await account_client.client_sign_in_one({ email, password })
-
-		}
-
-		const sign_in_two = async function({ answer, salt, encrypted_profile }) {
-
-			// complete account sign in
-			const profile_id = await account_client.client_sign_in_two({ answer, salt, encrypted_profile })
-
-			// sign in profile
-			return await profile_client.client_sign_in({ profile_id, answer })
-		}
-
-		// sign in
-		const si = await sign_in_one({ email, password })
-		si.answer = answer
-		delete si.question
-		const { jwt } = await sign_in_two(si)
+		const si1 = await api_client.sign_in_one({ email, password })
+		const si2 = await api_client.sign_in_two({ answer, salt: si1.salt, encrypted_profile: si1.encrypted_profile })
+		const { jwt } = await api_client.sign_in_three({ profile_id: si2, answer })
 
 		// verify jwt
-		await expect(profile_client.verify({ jwt })).resolves
+		await expect(api_client.verify_jwt({ jwt })).resolves
+
+		// TODO: more tests for logged in actions
 
 		/* sign out */
 
 		// sign out
-		expect(await profile_client.sign_out({ jwt })).resolves
+		expect(await api_client.sign_out({ jwt })).resolves
 
 		// verify jwt no longer works
-		await expect(profile_client.verify({ jwt })).rejects.toThrow(profile_client.errors.token_dne)
+		await expect(api_client.verify_jwt({ jwt })).rejects.toThrow(errors.token_dne)
 	}, 50000)
 
 })
