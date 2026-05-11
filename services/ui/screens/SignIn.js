906b007fc4b80b11b1ab3b02906e3f7007c595ca
AluminumOxide <sapphyre.gervais+git@gmail.com>
2026-05-11 13:51:15 -0400

    issue 61: sign in/up screens

--- services/ui/screens/SignIn.js
+++ services/ui/screens/SignIn.js
@@ -1,21 +1,54 @@
-import { useContext } from 'react';
-import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Form, PageContainer } from '../components/'
-import { AuthContext } from '../contexts/';
+import { FormView } from '../components/'
+import { AuthContext, FormContext } from '../contexts/';
 const api = require('@aluminumoxide/direct-democracy-external-api-client')
 
 export default function SignInScreen() {
 
 	const navigation = useNavigation();
-
 	const { setAuthState } = useContext(AuthContext)
+	const { defns, setDefns, setValue } = useContext(FormContext)
+	const [ signIn, setSignIn ] = useState({})
 
-	const handleProceed = async function({ jwt }) {
-		
-		const { profile_id: profile } = await api.verify_jwt({ jwt })
+	useEffect(() => {
+		setValue({ email: false, password: false})
+		setDefns({
+			email: {
+				title: 'Email',
+				format: 'string'
+			},
+			password: {
+				title: 'Password',
+				format: 'string'
+			},
+			answer: {
+				title: 'TODO',
+				format: 'string',
+				visible: false
+			}
+		})
+	}, [])
 
+	const handleProceed = async function(vals) {
+
+		// first sign in step
+		if(!vals.answer) {
+			const { email, password } = vals
+			const { question, salt, encrypted_profile } = await api.sign_in_one({ email, password })
+			setSignIn({ salt, encrypted_profile })
+			let copy = Object.assign({}, defns)
+			copy.answer.title = question
+			copy.answer.visible = true
+			setDefns(copy)
+			return false
+		}
+
+		// continue sign in
+		const { answer } = vals
+		const { salt, encrypted_profile } = signIn
+		const profile_id = await api.sign_in_two({ answer, salt, encrypted_profile })
+		const { jwt } = await api.sign_in_three({ profile_id, answer })
 		const mems = await api.membership_list({ jwt })
 		const root = await api.democracy_root()
 
@@ -25,36 +58,19 @@ export default function SignInScreen() {
 		setAuthState({
 			state: true,
 			jwt,
-			profile,
+			profile: profile_id,
 			root: root.democracy_id,
 			memberships
 		});
 
-		return navigation.navigate('Account')
+		return {}
 	}
 
-	return (<PageContainer
-		 title={'Sign In'}
-		 contents={
-			<View style={{flexDirection:'row', justifyContent:'center', padding:20}}>
-			<View style={{flexDirection:'column'}}>
-			<Text variant='bodyLarge'>To get a JWT, please run the following in a terminal:</Text>
-			<View style={{backgroundColor: 'black', padding: 10}}>
-			<Text style={{color: 'white', fontFamily:'monospace'}}>npm install -g @aluminumoxide/direct-democracy-authorizer</Text>
-			<Text style={{color: 'white', fontFamily:'monospace'}}>direct-democracy-authorizer</Text>
-			</View>
-			<Form
-		 	 formFields={['jwt']}
-		 	 reqFields={['jwt']}
-		 	 submit={handleProceed}
-		 	 fields={{
-		 	         jwt: {
-		 	        	 title: 'JWT',
-		 	        	 format: 'string'
-		 	         }
-		 	 }}/>
-			 </View>
-			 </View>
-		 }
-		/>)
+	return(<FormView
+		title={'Sign In'}
+		proceed={handleProceed}
+		nextScreen={'Account'}
+		formFields={['email','password','answer']}
+		reqFields={['email','password']}
+		fields={{}}/>)
 }
